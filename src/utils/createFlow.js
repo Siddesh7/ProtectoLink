e3083875ff9dae7f3cf2ee8990fc5464eb07f896
Siddesh <siddeshsankhya@gmail.com>
2023-07-24 11:22:00 +0530

    sf fucntions

---
+++ src/utils/createFlow.js
@@ -0,0 +1,16 @@
+import { ethers } from "ethers";
+
+export function calculateFlowRate(amountInEther) {
+  if (
+    typeof Number(amountInEther) !== "number" ||
+    isNaN(Number(amountInEther)) === true
+  ) {
+    console.log(typeof Number(amountInEther));
+    alert("You can only calculate a flowRate based on a number");
+    return;
+  } else if (typeof Number(amountInEther) === "number") {
+    const monthlyAmount = ethers.utils.parseEther(amountInEther.toString());
+    const calculatedFlowRate = Math.floor(monthlyAmount / 3600 / 24 / 30);
+    return calculatedFlowRate;
+  }
+}
